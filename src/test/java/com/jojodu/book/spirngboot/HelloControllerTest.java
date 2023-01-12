bf361db4e5aee4865d0718c9e27db377feaea98f
choum <choum9797@naver.com>
2023-01-12 23:59:36 +0900

    Hello controller 테스트코드 작성하기

---
+++ src/test/java/com/jojodu/book/spirngboot/HelloControllerTest.java
@@ -0,0 +1,33 @@
+package com.jojodu.book.spirngboot;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired	// {4}
+    private MockMvc mvc;
+
+    @Test
+    public void hello_to_return() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+
+    }
+
+}
