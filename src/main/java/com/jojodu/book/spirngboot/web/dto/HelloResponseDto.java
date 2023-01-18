34888ffb0b5fbb905943d40c2db5254cec1e6d6f
choum <choum9797@naver.com>
2023-01-18 22:00:54 +0900

    lombok 전환 및 테스트 코드 작성 확인
    1. 책에 있는거랑 인텔이제이 버전이 달라서 오류난 것, 그레들 버전에 맞춰서 롬복 수정
    2.helloDto 리턴되는거 확인했습니다.

--- src/main/java/com/jojodu/book/spirngboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/jojodu/book/spirngboot/web/dto/HelloResponseDto.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 //Getter : 선언된 모든 필드 get 메소드 생성
-//RequiredArgsConstructor :
+//RequiredArgsConstructor : 선언된 모든 final필드가 포함된 생성자를 생성, final이 없는 필드는 생성자 포함 안함
 @Getter
 @RequiredArgsConstructor
 public class HelloResponseDto {
---
+++ src/test/java/com/jojodu/book/spirngboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package com.jojodu.book.spirngboot.web.dto;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+public class HelloResponseDtoTest {
+
+    @Test
+    public void Lombok_Function_Test() {
+        // given
+        String name = "test";
+        int amount = 1000;
+
+        // when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        // then
+        assertThat(dto.getName()).isEqualTo(name);  //assertThat : 검증 메소드임, 메소드를 인자값으로 받음
+        assertThat(dto.getAmount()).isEqualTo(amount);// isEqualTo : 값 비교해서 같을 때만 성공
+    }
+
+}
