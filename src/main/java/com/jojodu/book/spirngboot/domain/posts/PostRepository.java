085a823327f90ddb54fe79b61c3ccf9a57826c0b
choum <choum9797@naver.com>
2023-01-19 20:18:32 +0900

    그레들빌더에 JPA랑 H2 설정, Posts클래스 생성 까지만
    다음 JPA 테스트코드 작성해야함

---
+++ src/main/java/com/jojodu/book/spirngboot/domain/posts/PostRepository.java
@@ -0,0 +1,9 @@
+package com.jojodu.book.spirngboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// Entity 클래스와 Entity Repository는 함께 있어야 한다
+// Entity는 기본 Repository가 없이 재대로 역할 불가
+public interface PostRepository extends JpaRepository<Posts, Long> {
+
+}
---
+++ src/main/java/com/jojodu/book/spirngboot/domain/posts/Posts.java
@@ -0,0 +1,43 @@
+package com.jojodu.book.spirngboot.domain.posts;
+
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+//Posts는 실제 DB 테이블과 매칭될 클래스, Entity클래스라고도 함
+/*
+* Entity : 테이블과 링크될 클래스임을 나타냄, 언더스코어 네이밍으로 한다함
+*          Entity클래스는 Setter 메소드 만들지 않는다함
+*          책에서는 값 변경이 필요하면 명확한 목적과 의도 알수 있는 메소드 추가를 권고함 - 92page 예시 있음
+* GeneratedValue : PK 생설 규칙 나타냄
+* Column : 굳이 선언 안해도 해당 클래스 필드는 모두 컬럼, 문자열은 기본 Varchar(255)가 기본
+* */
+
+@NoArgsConstructor
+@Entity
+public class Posts {
+    
+    // 부트 2.0버전에서는 GenerationType.IDENTITY해야 오토인크리먼트 된다함
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // 해당 테이블의 PK필드
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+ 
+
+}
