90d329cd6ce4cba4d3eda44f825f819d8a6aecae
choum <choum9797@naver.com>
2023-01-26 23:23:43 +0900

    수정 Api 및 테스트 코드 작성 확인
    1. Posts_수정된다 테스트 코드 확인
    2.102p~114p 확인

--- src/main/java/com/jojodu/book/spirngboot/service/posts/PostsService.java
+++ src/main/java/com/jojodu/book/spirngboot/service/posts/PostsService.java
@@ -1,8 +1,13 @@
 package com.jojodu.book.spirngboot.service.posts;
+
+import com.jojodu.book.spirngboot.domain.posts.Posts;
 import com.jojodu.book.spirngboot.domain.posts.PostsRepository;
+import com.jojodu.book.spirngboot.web.dto.PostsResponseDto;
 import com.jojodu.book.spirngboot.web.dto.PostsSaveRequestDto;
+import com.jojodu.book.spirngboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 import javax.transaction.Transactional;
 
 //RequiredArgsConstructor : 생성자를 스프링 @Autowired와 동일한 효과라서 해결해줌
@@ -16,5 +21,23 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
     }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                        .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id : " + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id : " + id));
+
+        return new PostsResponseDto(entity);
+    }
+
 
 }
