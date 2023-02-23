feaa44b04f548bd830d90fbb006ac9461450e70d
choum <choum9797@naver.com>
2023-02-23 21:39:51 +0900

    postapi테스트 오류 해결, 시큐리티 파일 설정 때문에 postApi테스트에서 오류 발생

--- src/main/java/com/jojodu/book/spirngboot/config/SecurityConfig.java
+++ src/main/java/com/jojodu/book/spirngboot/config/SecurityConfig.java
@@ -17,15 +17,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .headers()
+                .headers().frameOptions().disable()
                 //.frameOptions().disable()//h2 콘솔 화면을 사용하기 위해서 disable
                 .and()
-
                     .authorizeRequests()// 권한 관리를 설정하는 옵션의 시작점, authorizeRequests가 선언되어야 antMatchers 사용가능
                     //antMatchers : 권한관리 대상 옵션, url, http 메소드별로 관리 가능
                     .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll() // permitAll() : 전체 열람 권한 준 것
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name()) // /api/v1/** 주소를 가진 API는 user 권한을 가진 사람만 가능하도록
-                    .antMatchers("/api/v1/**").hasRole(Role.GUEST.name()) // 편하게 글 수정 작성하려고 임시로 추가함
                     .anyRequest().authenticated() // 나머지 url은 모두 인증된 사용자들(로그인한 사용자들)에게만 허용 가능 / anyRequest : 설정된 값들 외 나머지 url
                 .and()
                     .logout()// 로그아웃 기능에 대한 여러 설정의 진입점
